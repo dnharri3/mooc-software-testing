69d3b8c0d51fe08dd1d960e12b7fd94aea82d8d3
dnharri3 <56196417+dnharri3@users.noreply.github.com>
2019-10-08 19:38:57 -0400

    Complete test class for CaesarShiftCipher

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -11,8 +11,7 @@ public class CaesarShiftCipher {
 
         for(int i = 0; i < length; i++){
             currentChar = message.charAt(i);
-           
-            sb.append(currentChar);
+
             if (currentChar > 'z' || currentChar < 'a') {
                 return "invalid";
             } else if ((char) (currentChar + shift) > 'z') {
@@ -20,7 +19,8 @@ public class CaesarShiftCipher {
             } else if ((char) (currentChar + shift) < 'a'){
                 currentChar = (char) (currentChar + 26);
             }
-            sb.append((char) (currentChar + shift));
+                sb.append((char) (currentChar + shift));
+
         }
 
         return sb.toString();
--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,16 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CaesarShiftCipherTest {
+    @ParameterizedTest(name = "phrase={0}, number={1}, result={2}")
+    @CsvSource({"{,0,invalid","3,p,invalid"," ,5,invalid", "xyz,3,abc", "abc,3,def", "~,3,invalid","m, ,invalid"})
+    public void Algorithm1(String item, int num, String expectedResult){
+        String result = new CaesarShiftCipher().CaesarShiftCipher(item,num);
+        Assertions.assertEquals(expectedResult, result);
+    }
 
 }
