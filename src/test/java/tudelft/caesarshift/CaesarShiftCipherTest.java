80faea4f5df730650d27f72da40a53cd4fc065ad
dnharri3 <56196417+dnharri3@users.noreply.github.com>
2019-10-10 13:25:51 -0400

    Add more test cases to CeasarShiftJava

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class CaesarShiftCipherTest {
     @ParameterizedTest(name = "phrase={0}, number={1}, result={2}")
-    @CsvSource({"yz{,0,invalid","ABC,5,invalid", "xyz,3,abc", "abc,3,def", "~abc,3,invalid","XYZ,10,invalid"})
+    @CsvSource({"{z,0,invalid", "a,-1,invalid","xyz,3,invalid","abc,-3,invalid","^_~,3,invalid",
+                "ABC,5,invalid", "xyz,3,abc", "abc,3,def","lmn,50,jkl","pqr,78,pqr", "}|{,-6,invalid","XYZ,10,invalid"})
     public void Algorithm1(String item, int num, String expectedResult){
         String result = new CaesarShiftCipher().CaesarShiftCipher(item,num);
         Assertions.assertEquals(expectedResult, result);
